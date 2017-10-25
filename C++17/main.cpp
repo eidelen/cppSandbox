e4f3eda8c9e1c0d163261bd9ccf6389b29d6d872
Eidelen Yoga <adrian.schneider@unibas.ch>
2017-10-25 17:46:29 +0200

    Try C++17 std::variant

--- C++17/main.cpp
+++ C++17/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+
 #include <string_view>
 #include <string>
 
-void recvAStringView( std::string_view sv)
+void recvAStringView(std::string_view sv)
 {
     std::cout << "recv: " << sv << std::endl;
 }
 
-int main()
+void tryStringView()
 {
-    // try string_view
+    std::cout << "Try string view:" << std::endl;
+
     std::string_view sv("String view content");
     const char* charArr = "Char array content";
     std::string normalString("String content");
@@ -17,6 +19,45 @@ int main()
     recvAStringView(sv);
     recvAStringView(charArr);
     recvAStringView(normalString);
+
+    std::cout << std::endl;
+}
+
+
+
+#include <variant>
+#include <exception>
+
+void tryVariant()
+{
+    std::cout << "Try variant:" << std::endl;
+
+    std::variant<float, std::string, int> v0;
+
+    v0 = "abc";
+    std::cout << "v0, read string = " << std::get<std::string>(v0) << std::endl;
+    try
+    {
+        std::get<float>(v0);
+    }
+    catch( std::exception& e)
+    {
+        std::cout << "v0, get float not possible: " << e.what() << std::endl;
+    }
+
+    v0 = 3.0f;
+    std::cout << "v0, get float after set float: " << std::get<float>(v0) << std::endl;
+
+    std::cout << std::endl;
+}
+
+
+
+int main()
+{
+    tryStringView();
+    tryVariant();
+
 
     return 0;
 }
