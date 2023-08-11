e97d17688146d55a2a54493bb7cb35952940af60
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-08-11 17:23:30 +0200

    lookup

--- codewars/color_triangle.cpp
+++ codewars/color_triangle.cpp
@@ -9,13 +9,12 @@
 // product is order invariant
 constexpr char GB = 'G'*'B';
 constexpr char GR = 'G'*'R';
+constexpr char RB = 'R'*'B';
 
 constexpr char P = 0b00; // padding
 constexpr char R = 0b01; // red
 constexpr char G = 0b10; // green
 constexpr char B = 0b11; // blue
-
-
 
 std::string decompress1in4(char data)
 {
@@ -30,7 +29,7 @@ std::string decompress1in4(char data)
     return decompressed;
 }
 
-char compress4in1(const char* data)
+unsigned char compress4in1(const char* data)
 {
     char compressed = 0x00;
     for(int i = 0; i < 4; i++)
@@ -39,8 +38,8 @@ char compress4in1(const char* data)
         char code = a=='R' ? R : ( a=='G' ? G : ( a=='B' ? B : P));
         compressed = compressed | (code << (6 - i*2));
 
-        std::bitset<8> y(compressed);
-        std::cout << y << std::endl;
+        //std::bitset<8> y(compressed);
+        //std::cout << y << std::endl;
     }
 
     return compressed;
@@ -78,38 +77,40 @@ char getMissing(char a, char b)
     return 'G';
 }
 
-void getMissingInplace(char* aAdr)
-{
-    char a = *aAdr;
-    char b = *(aAdr+1);
-
-    if(a == b)
-        return;
-
-    const char prod = a*b;
-    if(prod == GB)
-        *aAdr = 'R';
-    else if(prod == GR)
-        *aAdr = 'B';
-    else
-        *aAdr = 'G';
-}
-
 char triangle(std::string row_str)
 {
+    // remove padding
+    row_str.erase(std::remove(row_str.begin(), row_str.end(), '-'), row_str.end());
+    if(row_str.length() == 0)
+        return '-';
+
     for(size_t k = row_str.length(); k > 0; k--)
     {
         for(size_t i = 0; i < k-1; i++)
         {
-            getMissingInplace(&row_str[i]); //getMissing(row_str[i], row_str[i+1]);
+            row_str[i] = getMissing(row_str[i], row_str[i+1]);
         }
     }
     return row_str[0];
 }
 
+std::vector<char> createLookup()
+{
+    std::vector<char> table(255);
+    for(size_t code = 0; code < 255; code++)
+    {
+        std::string config = decompress1in4((char)code);
+        char config_res = triangle(config);
+        table[code] = config_res;
+    }
+
+    return table;
+}
+
 
 int main (int argc, char *argv[])
 {
+
     std::string q("RGRB");
     char compTest = compress4in1(&q[0]);
     std::string decompTest = decompress1in4(compTest);
@@ -121,12 +122,20 @@ int main (int argc, char *argv[])
     std::bitset<8> b1(compLine[1]);
     std::cout << q << ": compressed to n chars = " << compLine.size() << ", bytes=" << b0 << ", " << b1 << std::endl;
 
+    std::vector<char> lt = createLookup();
+    std::cout << "LookupTable: " << lt[(unsigned char)compress4in1("GGGG")] << std::endl;
+    std::cout << "LookupTable: " << lt[(unsigned char)compress4in1("BBBR")] << std::endl;
+    std::cout << "LookupTable: " << lt[(unsigned char)compress4in1("BB--")] << std::endl;
 
-    char res = triangle("RBRGBRB");
-    if(res == 'G')
+
+    char res = triangle("--RBRGR-"); // padding removed
+    if(res == 'R')
         std::cout << "Fine" << std::endl;
     else
         std::cout << "Wrong!!" << std::endl;
+
+    res = triangle("BBBR");
+    std::cout << "BBBR->" << res << std::endl;
 
     // long test
     std::string longStr(30000, 'B'); // Besttime 3s,
