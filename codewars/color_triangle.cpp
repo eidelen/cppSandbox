3a100b732c69913494b8b98cc96b11ef84a3a8c5
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-08-12 11:01:20 +0200

    Compressed.. but too slow

--- codewars/color_triangle.cpp
+++ codewars/color_triangle.cpp
@@ -120,8 +120,15 @@ std::vector<char> createLookup()
 
 char triangle(std::string row_str)
 {
+
+    std::chrono::steady_clock::time_point prepare = std::chrono::steady_clock::now();
+
     std::vector<char> lt = createLookup();
     std::vector<char> cLine = compressLine(row_str);
+
+    std::chrono::steady_clock::time_point endPreapare = std::chrono::steady_clock::now();
+    std::cout << "Preparation duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(endPreapare - prepare).count() << std::endl;
+
 
     // process three lines at once
     for(size_t k = 0; k < row_str.length()/3; k++)
@@ -131,16 +138,16 @@ char triangle(std::string row_str)
         for(size_t j = 0; j < cLine.size(); j++)
         {
             short a = (((short)cLine[j]) << 8);
-            short b = (j < cLine.size() ) ? ((short)cLine[j+1]) & 0x00FF : 0x0000; // padding the last element of the line
+            short b = (j < cLine.size()-1 ) ? ((short)cLine[j+1]) & 0x00FF : 0x0000; // padding the last element of the line
 
             // combine into short
             short comb = a | b;
 
-
+            /*
             std::bitset<16> ck(comb);
             std::bitset<16> ca(a);
             std::bitset<16> cb(b);
-            std::cout << "Traverse: " << ck << ", a=" << ca << ", b=" << cb << std::endl;
+            std::cout << "Traverse: " << ck << ", a=" << ca << ", b=" << cb << std::endl;*/
 
 
             std::string res(4, '-');
@@ -150,17 +157,17 @@ char triangle(std::string row_str)
                 char evalCode = lt[(unsigned char)theCode];
                 res[step] = evalCode;
 
-                //std::bitset<8> tc(theCode);
-                //std::cout << "Step: (" << j << "," << step << "): " << tc << "=" << decompress1in4(theCode) << ", eval=" << evalCode << std::endl;
+                /*std::bitset<8> tc(theCode);
+                std::cout << "Step: (" << j << "," << step << "): " << tc << "=" << decompress1in4(theCode) << ", eval=" << evalCode << std::endl;*/
 
             }
 
             char resCompress = compress4in1(&res[0]);
-            if(resCompress != 0x00) // do not store padding
+            if(resCompress != 0x00) // do not store padding blocks
             {
                 storeRes.push_back(resCompress);
-                std::bitset<8> stepResPrint(resCompress);
-                std::cout << "Steps Res: (" << j << "): " << stepResPrint << ", size res vect: " << storeRes.size() << std::endl;
+                /*std::bitset<8> stepResPrint(resCompress);
+                std::cout << "Steps Res: (" << j << "): " << stepResPrint << ", size res vect: " << storeRes.size() << std::endl;*/
             }
         }
 
@@ -169,6 +176,8 @@ char triangle(std::string row_str)
 
     // naive approach for the 1 - 3 remaining lines
     std::string finalLine = decompress1in4(cLine[0]);
+
+    std::cout << "Final Line: " << finalLine.length() << std::endl;
     finalLine.erase(std::remove(finalLine.begin(), finalLine.end(), '-'), finalLine.end()); // remove any padding character
     return triangleNaive(finalLine);
 }
@@ -176,11 +185,26 @@ char triangle(std::string row_str)
 
 int main (int argc, char *argv[])
 {
-    std::string test("GBGBB"); // failing -> wrong encoded?
+    std::string test("BGRRRRG");
 
     char shouldRes = triangleNaive(test);
     std::cout << test << " evaluates to " << shouldRes << std::endl;
     std::cout << "Fast solution: " << triangle(test) << std::endl;
+
+
+    // long test
+    std::string longStr(30000, 'G'); // Besttime 3s,
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    char res = triangle(longStr);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    if(res == 'G')
+        std::cout << "Fine" << std::endl;
+    else
+        std::cout << "Wrong!!" << std::endl;
+
+    std::cout << "Duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+
+
 
     /*
     constexpr char P = 0b00; // padding
