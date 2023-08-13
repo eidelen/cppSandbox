5552a7fd367c751f25d8930f0e63939bfc123d07
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-08-13 21:34:54 +0200

    try to see pattern

--- codewars/color_triangle.cpp
+++ codewars/color_triangle.cpp
@@ -90,12 +90,16 @@ char triangleNaive(std::string row_str)
     if(row_str.length() == 1)
         return row_str[0];
 
+    std::cout << row_str << std::endl;
+
     for(size_t k = row_str.length(); k > 0; k--)
     {
         for(size_t i = 0; i < k-1; i++)
         {
             row_str[i] = getMissing(row_str[i], row_str[i+1]);
         }
+
+        std::cout << row_str.substr(0, k-1) << std::endl;
     }
     return row_str[0];
 }
@@ -103,7 +107,7 @@ char triangleNaive(std::string row_str)
 std::unordered_map<std::string, char> createLookup()
 {
     std::unordered_map<std::string, char> table;
-    std::string config(4, '-');
+    std::string config(10, '-');
     for(char i: {'R','B', 'G', '-'})
     {
         config[0] = i;
@@ -116,12 +120,34 @@ std::unordered_map<std::string, char> createLookup()
                 for(char n: {'R','B', 'G', '-'})
                 {
                     config[3] = n;
+                    for(char o: {'R','B', 'G', '-'})
+                    {
+                        config[4] = o;
+                        for(char p: {'R','B', 'G', '-'})
+                        {
+                            config[5] = p;
+                            for(char q: {'R','B', 'G', '-'})
+                            {
+                                config[6] = q;
+                                for(char r: {'R','B', 'G', '-'})
+                                {
+                                    config[7] = r;
+                                    for(char s: {'R','B', 'G', '-'})
+                                    {
+                                        config[8] = s;
+                                        for(char t: {'R','B', 'G', '-'})
+                                        {
+                                            config[9] = t;
+                                            char solution = triangleNaive(config);
+                                            //std::cout << "Table: " << solution << ":" << config << std::endl;
 
-                    char solution = triangleNaive(config);
-                    //std::cout << "Table: " << solution << ":" << config << std::endl;
-
-                    table[config] = solution;
-
+                                            table[config] = solution;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -137,10 +163,9 @@ char triangle(std::string row_str)
 
     std::unordered_map<std::string, char> lt = createLookup();
 
-    std::cout << "LU: BGRR " << lt["BGRR"] << std::endl;
 
     // line padding
-    size_t paddingLength = row_str.length() % 4 == 0 ? 0 : 4 - (row_str.length() % 4);
+    size_t paddingLength = row_str.length() % 10 == 0 ? 0 : 10 - (row_str.length() % 10);
     row_str.append(paddingLength, '-');
 
     std::string nextLine = row_str;
@@ -148,20 +173,24 @@ char triangle(std::string row_str)
     std::chrono::steady_clock::time_point endPreapare = std::chrono::steady_clock::now();
     std::cout << "Preparation duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(endPreapare - prepare).count() << std::endl;
 
-    // process three lines at once
-    for(size_t k = 0; k < row_str.length()/3; k++)
+    size_t nbrComputations = 0;
+
+    // process 7 lines at once
+    for(size_t k = 0; k < (row_str.length()/9)-1; k++)
     {
-        std::string resLine(nextLine.length()-3, '-'); // init with length
+        std::string resLine(nextLine.length()-9, '-'); // init with length
 
         //std::cout << k << ": NextLine: " << nextLine << std::endl;
 
-        for(size_t j = 0; j < nextLine.length()-3; j++) // process 4 items at once
+        for(size_t j = 0; j < nextLine.length()-9; j++) // process 7 items at once
         {
-            std::string config = nextLine.substr(j, 4);
+            std::string config = nextLine.substr(j, 10);
             char res = lt[config];
             resLine[j] = res;
 
             //std::cout << j << ": Config = " << config << ", res = " << res << std::endl;
+
+            nbrComputations++;
         }
 
         nextLine = resLine;
@@ -170,7 +199,7 @@ char triangle(std::string row_str)
     // naive approach for the 1 - 3 remaining lines
     std::string finalLine = nextLine;
 
-    std::cout << "Final Line: " << finalLine.length() << std::endl;
+    std::cout << "Final Line: " << finalLine.length() <<", NbrComp: " << nbrComputations << std::endl;
     finalLine.erase(std::remove(finalLine.begin(), finalLine.end(), '-'), finalLine.end()); // remove any padding character
     return triangleNaive(finalLine);
 }
@@ -178,15 +207,25 @@ char triangle(std::string row_str)
 
 int main (int argc, char *argv[])
 {
-    std::string test("BGRRRRG");
+    std::string test("BBGRBGRB");
+
+    /*
+     *  BB -> B
+     *  BB -> B
+     *  RR -> R
+     *   R -> B
+     *
+     *
+     *
+     */
 
     char shouldRes = triangleNaive(test);
-    std::cout << test << " evaluates to " << shouldRes << std::endl;
-    std::cout << "Fast solution: " << triangle(test) << std::endl;
+    //std::cout << test << " evaluates to " << shouldRes << std::endl;
+    //std::cout << "Fast solution: " << triangle(test) << std::endl;
 
-
+/*
     // long test
-    std::string longStr(30000, 'G'); // Besttime 3s, 2.4s
+    std::string longStr(30000, 'G'); // Besttime 3s, 2.4s, 4Hash = 19s and 149985000, 8Hash = 8s 64270710
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     char res = triangle(longStr);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -197,7 +236,7 @@ int main (int argc, char *argv[])
 
     std::cout << "Duration ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
 
-
+*/
 
     /*
     constexpr char P = 0b00; // padding
