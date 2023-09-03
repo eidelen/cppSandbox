063131079f24e0fa66e20b6b38d6a441941b90e6
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-09-03 20:13:21 +0200

    finished next bigger

--- codewars/nextbigger.cpp
+++ codewars/nextbigger.cpp
@@ -4,58 +4,6 @@
 #include <numeric>
 #include <cmath>
 
-static size_t leaves = 0;
-static size_t reccnt = 0;
-
-long rabbitHole(const long nbr, const long sum, const long pos, std::vector<long> digits, long& bestSolution)
-{
-    if(pos < 0)
-    {
-        leaves++;
-        return sum;
-    }
-
-
-    // pruning recursion
-    auto[minE, maxE] = std::minmax_element(digits.begin(), digits.end());
-    long lowerBound = sum + *minE * std::pow(10, pos);
-    long upperBound = sum + (*maxE) * std::pow(10, pos) + (std::pow(10, pos)-1);
-    if( upperBound <= nbr && upperBound > 0) //overflow!
-        return -1;
-    if( lowerBound > bestSolution && bestSolution != -1)
-        return -1;
-
-
-    //std::cout << pos << ": " << sum << ", " << ", lb=" << lowerBound << ", ub=" << upperBound << std::endl;
-
-    long solution = -1;
-    for(size_t i = 0; i < digits.size(); i++)
-    {
-        long thenbr = digits[i];
-        long mulPos = std::pow(10l, pos);
-        long mulAdd = thenbr * mulPos;
-        long nextSum = sum + mulAdd;
-        std::vector<long> nextDigits(digits);
-        nextDigits.erase(nextDigits.begin() + i);
-        long pSol = rabbitHole(nbr, nextSum, pos-1, nextDigits, bestSolution);
-
-        if( (pSol > nbr) && (pSol < solution || solution < 0))
-        {
-            reccnt++;
-
-            solution = pSol;
-            //std::cout << pos << ": " <<"new solution: " << solution << ", reccnt: " << reccnt << std::endl;
-
-            if(solution < bestSolution || bestSolution < 0)
-            {
-                bestSolution = solution;
-                //std::cout << pos << ": " << "Best solution: " << bestSolution << std::endl;
-            }
-        }
-    }
-
-    return solution;
-}
 
 long nextBigger(long n)
 {
@@ -88,16 +36,14 @@ long nextBigger(long n)
 
 int main (int argc, char *argv[])
 {
-    long nbrIn = 59884848459853;
-    long resOut = 59884848483559;
+    long nbrIn = 599776611;
+    long resOut = 2071;
     long res = nextBigger(nbrIn);
 
     if(res == resOut)
         std::cout << "Correct: " << nbrIn << " -> " << res << std::endl;
     else
-        std::cout << "Wrong: " << nbrIn << " -> " << res << " should be " << resOut << std::endl;
-
-    std::cout << "nleaves=" << leaves << std::endl;
+        std::cout << "Wrong: " << nbrIn << " -> " << res << "should be " << resOut << std::endl;
 
     return 0;
 }
