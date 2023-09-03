db0231b44245dc9a0d39443e81257b74e7c014d2
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-09-03 13:16:16 +0200

    solution

--- codewars/nextbigger.cpp
+++ codewars/nextbigger.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 #include <cmath>
 
 static size_t leaves = 0;
@@ -58,56 +59,37 @@ long rabbitHole(const long nbr, const long sum, const long pos, std::vector<long
 
 long nextBigger(long n)
 {
+    // transform to vector
     std::string nStr = std::to_string(n);
     std::vector<long> digits(nStr.length(), 0);
     std::transform(nStr.begin(), nStr.end(), digits.begin(), [](char c) -> long { return c - 48; });
 
-    // swap next smaller element coming from the back
-    int swapPos = -1;
-    for(int m = digits.size()-1; m >= 0 && swapPos < 0; m--)
-    {
-        long sw1 = digits[m];
-        for(int n = m; n >= 0 && swapPos < 0; n--)
-        {
+    // swap next smaller element coming from the back -> best option has max n
+    std::pair<int, int> swapPos = {-1, -1};
+    for(int m = digits.size()-1; m >= 0; m--)
+        for(int n = m; n >= 0; n--)
+            if(digits[n] < digits[m])
+                if( n > swapPos.second )
+                    swapPos = {m, n};
 
-            long sw2 = digits[n];
+    if(swapPos.second == -1)
+        return -1; // no swap found
 
-            std::cout << "compare: " << sw2 << ", " << sw1 << std::endl;
+    std::swap(digits[swapPos.first], digits[swapPos.second]);
 
-            if(sw2 < sw1)
-            {
-                std::cout << "swap: " << sw2 << ", " << sw1 << std::endl;
-                std::swap(digits[n], digits[m]);
-                swapPos = n;
-            }
+    // sort remaining elements after swap position
+    std::sort(digits.begin()+swapPos.second+1, digits.end());
 
-        }
-    }
-
-    for(auto l: digits)
-        std::cout << l << ", ";
-    std::cout << std::endl;
-
-    std::cout << "swappos:" << swapPos << std::endl;
-
-    std::sort(digits.begin()+swapPos+1, digits.end());
-
-    for(auto l: digits)
-        std::cout << l << ", ";
-    std::cout << std::endl;
-
-    long res = 0;
-    for(int p = 0; p < digits.size(); p++)
-        res += digits[p] * (long)std::pow(10l, digits.size() - p - 1);
-
-    return res;
+    // accumulate
+    return std::accumulate(digits.begin(), digits.end(), 0l, [idx = 0, nbrElems = digits.size()](long sum, long val) mutable ->long {
+        return sum + val * (long)std::pow(10l, nbrElems - (idx++) - 1); });
 }
 
 
 int main (int argc, char *argv[])
 {
-    long nbrIn = 7799665555444321100;
-    long resOut = 7900112344455556679;
+    long nbrIn = 59884848459853;
+    long resOut = 59884848483559;
     long res = nextBigger(nbrIn);
 
     if(res == resOut)
