c03f16cf99b003fae74b4bd379db27dc0e8c717f
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-11-01 17:39:05 +0100

    coroutines

--- C++20/simple20.cpp
+++ C++20/simple20.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
+#include <coroutine>
+#include <concepts>
+
+//** Coroutines **/
+struct ReturnObject {
+    struct promise_type {
+        ReturnObject get_return_object() { return {}; }
+        std::suspend_never initial_suspend() { return {}; }
+        std::suspend_never final_suspend() noexcept { return {}; }
+        void unhandled_exception() {}
+        void return_void() noexcept {}
+    };
+};
+
+struct Awaiter
+{
+    std::coroutine_handle<> *hp_;
+    constexpr bool await_ready() const noexcept { return false; }
+    void await_suspend(std::coroutine_handle<> h) { *hp_ = h; }
+    constexpr void await_resume() const noexcept {}
+};
+
+ReturnObject counter(std::coroutine_handle<> *continuation_out)
+{
+    Awaiter a{continuation_out};
+    for (unsigned i = 0;; ++i) // Does not end
+    {
+        co_await a;
+        std::cout << "counter: " << i << std::endl;
+    }
+}
+
+//******
 
 
 int main()
@@ -16,6 +49,20 @@ int main()
         else
             std::cout << "k and q are equal" << std::endl;
     }
+
+    {
+        // coroutines
+        std::coroutine_handle<> h;
+        counter(&h);
+        for (int i = 0; i < 3; ++i)
+        {
+            std::cout << "In main1 function\n";
+            h();
+        }
+        h.destroy();
+    }
+
+
 
     return 0;
 }
